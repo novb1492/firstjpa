56a3b02c1aab5925a553ff111ce897116b433772
novb1492 <novb1492@naver.com>
2021-05-14 14:42:54 +0900

    ajax로 아이디 중복확인 너무 신난다

--- demo/src/main/java/com/example/demo/controller/confirm.java
+++ demo/src/main/java/com/example/demo/controller/confirm.java
@@ -1,6 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+import com.example.demo.usermodel.uservo;
+import com.example.demo.userservice.iuser;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +16,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class confirm {
-    
+    @Autowired
+    private iuser iuser;
     @PostMapping("comfirm")
-    public String postMethodName(@RequestParam("userid")String id) {
-        System.out.println(id+"userid");
+    public String postMethodName(uservo user) {
         
-        return "yes";
+        System.out.println(user.getid()+"userid");
+        
+        Optional<uservo> vo=iuser.findById(user.getid());
+           if(vo.isEmpty())///일단 학원가기전까지는 이방법이 제일 편리 한거같다 20200514
+           {
+                return "yes";
+           }
+        return "no";
     }
     
 }
