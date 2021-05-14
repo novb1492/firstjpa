9d07a74cf953826d29993751e7eccea68c6de217
novb1492 <novb1492@naver.com>
2021-05-14 14:52:13 +0900

    나머지는 집가서

--- demo/src/main/java/com/example/demo/controller/confirm.java
+++ demo/src/main/java/com/example/demo/controller/confirm.java
@@ -6,12 +6,9 @@ import java.util.Optional;
 
 import com.example.demo.usermodel.uservo;
 import com.example.demo.userservice.iuser;
-
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -20,11 +17,11 @@ public class confirm {
     private iuser iuser;
     @PostMapping("comfirm")
     public String postMethodName(uservo user) {
-        
+
         System.out.println(user.getid()+"userid");
         
         Optional<uservo> vo=iuser.findById(user.getid());
-           if(vo.isEmpty())///일단 학원가기전까지는 이방법이 제일 편리 한거같다 20200514
+           if(vo.isEmpty()&&user.getid()!=null)///일단 학원가기전까지는 이방법이 제일 편리 한거같다 20200514
            {
                 return "yes";
            }
