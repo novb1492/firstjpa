28d7e074752252c23b20403e6251565a1934a270
novb1492 <novb1492@naver.com>
2021-05-22 17:25:43 +0900

    service화시키는중

--- demo/src/main/java/com/example/demo/service/boardservice/boardservice.java
+++ demo/src/main/java/com/example/demo/service/boardservice/boardservice.java
@@ -7,8 +7,9 @@ import com.example.demo.boradvo.boardvo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 
@@ -20,8 +21,14 @@ public class boardservice {
     
     public Page<boardvo> getboardlist(int pageNum) {
         Page<boardvo>array =boarddao.findAll(PageRequest.of(pageNum-1, 3,Sort.by(Sort.Direction.DESC, "bid")));
-        System.out.println(array);
-        return array;
+        System.out.println(array);///// 진짜 페이징이 이렇게 편하게된다
+        return array; 
+    }
+
+    public String getsessionemail() {
+        Object principal=SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            UserDetails userDetails=(UserDetails)principal;
+        return userDetails.getUsername();
         
     }
 }
